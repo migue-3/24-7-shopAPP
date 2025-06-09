62f2d292743ce3012901f378f86987b584848f12
escalante <miguel.escalante@accenture.com>
2025-06-09 13:58:35 -0300

    continuar proyecto en equipo propio

---
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {}
