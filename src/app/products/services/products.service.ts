71dd424946d6c46abaec75f845de4363cac35619
Miguel Escalante <miguelgervis1@gmail.com>
2025-11-04 16:38:50 -0300

    resolviendo error en los environments

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -7,7 +7,7 @@ import {
   ProductsResponse,
 } from '@products/interfaces/product.interface';
 import { forkJoin, map, Observable, of, switchMap, tap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
 
